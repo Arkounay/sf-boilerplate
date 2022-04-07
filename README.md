497bfd68a6023618f509fcf9a9944037276d381e
Antonin <arkanen@gmail.com>
2022-04-07 15:48:22 +0200

    nouveau label pour que le projet fonctionne de base + composer update

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ set project_name [ProjectName] \
 && git clone --depth=1 git@github.com:Arkounay/sf6-boilerplate.git $project_name \
 && cd $project_name \
 && sed -i "s/sf6-boilerplate/$project_name/" .env \
-&& sed -i "s/%project_name%/$project_name/g" src/Service/MailingService.php \
-&& sed -i "s/%project_name%/$project_name/g" config/packages/qag.yaml \
+&& sed -i "s/__project_name__/$project_name/g" src/Service/MailingService.php \
+&& sed -i "s/__project_name__/$project_name/g" config/packages/qag.yaml \
 && composer install \
 && rm -rf .git \
 && rm README.md \
--- src/Service/MailingService.php
+++ src/Service/MailingService.php
@@ -11,14 +11,14 @@ class MailingService
 {
 
     public const TO = 'samuel@plugandcom.com';
-    public const SUBJECT_PREFIX = '[%project_name%]';
+    public const SUBJECT_PREFIX = '[__project_name__]';
 
     public function __construct(private MailerInterface $mailer){}
 
     public function sendContact(Contact $contact): void
     {
         $email = (new TemplatedEmail())
-            ->from(new Address('%project_name%@plugandcom.com', '%project_name%'))
+            ->from(new Address('__project_name__@plugandcom.com', '__project_name__'))
             ->to(self::TO)
             ->replyTo($contact->getEmail())
             ->subject(self::SUBJECT_PREFIX . " Nouvelle demande de contact de $contact")
